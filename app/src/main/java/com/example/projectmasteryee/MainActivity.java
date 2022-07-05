3d3fb10a38a9a4fc0375774776bb0c980ea8345f
Blen <blenbre777@naver.com>
2022-07-05 15:08:19 +0900

    버튼 클릭시 네이버 이동 테스트

--- app/src/main/java/com/example/projectmasteryee/MainActivity.java
+++ app/src/main/java/com/example/projectmasteryee/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.projectmasteryee;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +17,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+    }
 
-
+    public void onButton(View v){
+        Intent n = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"));
+        startActivity(n);
     }
 }
